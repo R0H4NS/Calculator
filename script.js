3e8a329295776d555b881a085d3c058d13ee4751
R0H4NS <113116544+R0H4NS@users.noreply.github.com>
2024-05-09 14:06:08 -0400

    a

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ function display(value) {
     document.getElementById("screen").value += value;
 }
 
-function displaysqrt(value) {
-    document.getElementById("screen").Math.sqrt(value);
-}
-
 function calculate() {
     var p = document.getElementById("screen").value;
     var q = eval(p);
     document.getElementById("screen").value = q;
+}
+
+function deletes() {
+    var screen = document.getElementById("screen")
+    if(screen.value.length > 0)
+    {
+        screen.value = screen.value.substring(0, screen.value.length-1);
+    }
 }
